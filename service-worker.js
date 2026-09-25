3e3b0463afdd348a8eab4e6ca6a3a403f4d636f4
Lau <l.somacarrera@gmail.com>
2026-09-25 11:38:36 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,16 @@
+const CACHE_VERSION = "peso-v2";
+
 self.addEventListener("install", () => {
+  console.log("Instalando Service Worker:", CACHE_VERSION);
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker activo");
+self.addEventListener("activate", (event) => {
+  console.log("Activando Service Worker:", CACHE_VERSION);
+
+  event.waitUntil(
+    self.clients.claim()
+  );
 });
 
 self.addEventListener("fetch", () => {});
